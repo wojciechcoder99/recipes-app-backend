a63abd3f225fa7d3c9a0ff1b4a4f3c9adbd6873f
wojo321 <wojtek.f.99@gmail.com>
2022-03-29 00:21:57 +0200

    Resolved n+1 queries problem

--- src/main/java/com/courseapp/backend/repositories/RecipeRepository.java
+++ src/main/java/com/courseapp/backend/repositories/RecipeRepository.java
@@ -2,8 +2,14 @@ package com.courseapp.backend.repositories;
 
 import com.courseapp.backend.model.recipe.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface RecipeRepository extends IGenericRepository<Recipe, Long> {
+    @Override
+    @Query("from Recipe r join fetch r.ingredients")
+    List<Recipe> findAll();
 }
