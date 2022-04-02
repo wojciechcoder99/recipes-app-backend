1b95b608d378d8850ac9cd615d7d492734a34f0b
wojo321 <wojtek.f.99@gmail.com>
2022-04-02 16:51:47 +0200

    Finished Ingredient service tests

--- src/test/java/com/courseapp/backend/IngredientDataFactory.java
+++ src/test/java/com/courseapp/backend/IngredientDataFactory.java
@@ -2,19 +2,23 @@ package com.courseapp.backend;
 
 import com.courseapp.backend.model.ingredient.Ingredient;
 import com.courseapp.backend.model.ingredient.IngredientDTO;
-
-import java.util.Arrays;
-import java.util.List;
+import java.util.Collections;
 
 public final class IngredientDataFactory {
-    public static List<Ingredient> createIngredientList() {
-        return Arrays.asList(new Ingredient(1, "Mango",2));
+    private static final Ingredient testingIngredient = new Ingredient(1, "Apple", 3);
+    private static final IngredientDTO testingIngredientDTO = new IngredientDTO(1, "Apple", 3);
+
+    public static Ingredient createIngredient() {
+        return testingIngredient;
+    }
+    public static IngredientDTO createIngredientDTO() {
+        return testingIngredientDTO;
     }
 
-    public static Ingredient createIngredient(long id) {
-        return new Ingredient(id, "Apple", 3);
+    public static Iterable<IngredientDTO> createIngredientDTOsCollection() {
+        return Collections.singletonList(createIngredientDTO());
     }
-    public static IngredientDTO createIngredientDTO(long id) {
-        return new IngredientDTO(id, "Apple", 3);
+    public static Iterable<Ingredient> createIngredientsCollection() {
+        return Collections.singletonList(createIngredient());
     }
 }
--- src/test/java/com/courseapp/backend/services/ingredient/IngredientServiceImplTest.java
+++ src/test/java/com/courseapp/backend/services/ingredient/IngredientServiceImplTest.java
@@ -6,18 +6,26 @@ import com.courseapp.backend.repositories.IngredientRepository;
 import com.courseapp.backend.services.BaseServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static com.courseapp.backend.IngredientDataFactory.createIngredient;
-import static com.courseapp.backend.IngredientDataFactory.createIngredientDTO;
+import static com.courseapp.backend.IngredientDataFactory.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(SpringExtension.class)
 class IngredientServiceImplTest {
 
+    @Mock
     private ModelMapper modelMapper;
     @Mock
     private IngredientRepository ingredientRepository;
@@ -26,15 +34,14 @@ class IngredientServiceImplTest {
 
     @BeforeEach
     void setUp() {
-        modelMapper = mock(ModelMapper.class);
         ingredientService = new IngredientServiceImpl(modelMapper, ingredientRepository);
     }
 
     @Test
     void testConvertToEntity() {
         // given
-        IngredientDTO toConvert = createIngredientDTO(1);
-        Ingredient expected = createIngredient(1);
+        IngredientDTO toConvert = createIngredientDTO();
+        Ingredient expected = createIngredient();
         when(modelMapper.map(toConvert, Ingredient.class)).thenReturn(expected);
         // when
         Ingredient actual = ingredientService.convertToEntity(toConvert);
@@ -44,13 +51,37 @@ class IngredientServiceImplTest {
 
     @Test
     void testConvertToDTO() {
+        // given
+        Ingredient toConvert = createIngredient();
+        IngredientDTO expected = createIngredientDTO();
+        when(modelMapper.map(toConvert, IngredientDTO.class)).thenReturn(expected);
+        // when
+        IngredientDTO actual = ingredientService.convertToDTO(toConvert);
+        // then
+        assertEquals(expected, actual);
     }
 
     @Test
     void testConvertToCollectionOfEntities() {
+        // given
+        Iterable<IngredientDTO> toConvert = createIngredientDTOsCollection();
+        Iterable<Ingredient> expected = createIngredientsCollection();
+        when(ingredientService.convertToEntity(createIngredientDTO())).thenReturn(createIngredient());
+        // when
+        Iterable<Ingredient> actual = ingredientService.convertToCollectionOfEntities(toConvert);
+        // then
+        assertEquals(expected, actual);
     }
 
     @Test
     void testConvertToCollectionOfDTOs() {
+        // given
+        Iterable<Ingredient> toConvert = createIngredientsCollection();
+        Iterable<IngredientDTO> expected = createIngredientDTOsCollection();
+        when(ingredientService.convertToDTO(createIngredient())).thenReturn(createIngredientDTO());
+        // when
+        Iterable<IngredientDTO> actual = ingredientService.convertToCollectionOfDTOs(toConvert);
+        // then
+        assertEquals(expected, actual);
     }
 }
